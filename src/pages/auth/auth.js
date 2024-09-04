e1eda3aa324c13a59fab8272ede7ad6bb423fadb
Vladislav Fatikhov <90382910+fatikhoov@users.noreply.github.com>
2024-09-04 14:21:05 +0000

    commit

--- src/pages/auth/auth.js
+++ src/pages/auth/auth.js
@@ -1,9 +1,12 @@
+import TextField from '@mui/material/TextField';
+
 export default function AuthPage({onClick, children}) {
     
     return (
         <main className="App-main app-container">
          <p>Форма входа</p>
- 
+         <TextField id="outlined-login" label="Логин" variant="outlined" color="primary" focused />
+         <TextField id="outlined-password" label="Пароль" variant="outlined" color="primary" />
          <button className="main-add-working-shift" onClick={onClick}>
            {children}
          </button>
