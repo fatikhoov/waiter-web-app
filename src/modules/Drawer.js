e7b0a95b7a3f768debc3e3e2e16b9e4311dbeee6
Vladislav Fatikhov <90382910+fatikhoov@users.noreply.github.com>
2024-09-20 14:56:00 +0000

    commit

--- src/modules/Drawer.js
+++ src/modules/Drawer.js
@@ -4,10 +4,23 @@ import CssBaseline from '@mui/material/CssBaseline';
 import CustomAppBar from '../components/CustomAppBar';
 import CustomDrawer from '../components/CustomDrawer';
  
+import { AppProvider } from '@toolpad/core/AppProvider';
+import { useDemoRouter } from '@toolpad/core/internals';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '../theme/theme';
+import Paper from '@mui/material/Paper';
 import { Outlet } from 'react-router-dom'; // Импортируем Outlet
 
 export default function MiniDrawer({ auth, user, handleLogout, toggleTheme, isDarkMode }) {
+  const theme = useTheme(isDarkMode); // Получаем объект темы в зависимости от режима
  
+  const matches = useMediaQuery('(min-width:480px)');
+  const NAVIGATION = [
+    { segment: '', title: 'Главная' },
+    { segment: 'sign-in', title: 'Вход' },
+  ];
+  
+  const router = useDemoRouter('/sign-in');
   const [open, setOpen] = React.useState(false); 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -16,15 +29,19 @@ export default function MiniDrawer({ auth, user, handleLogout, toggleTheme, isDa
     setOpen(false);
   }; 
   return (
-    <Box sx={{ display: 'flex' }}>
+    
+    <AppProvider navigation={NAVIGATION} router={router} theme={theme}> 
       <CssBaseline />
 
       <CustomAppBar  open={open}  handleDrawerOpen={handleDrawerOpen} handleLogout={handleLogout} auth={auth}  user={user}  isDarkMode={isDarkMode}  toggleTheme={toggleTheme}  />
-      <CustomDrawer  open={open}  handleDrawerClose={handleDrawerClose} auth={auth}  user={user}  isDarkMode={isDarkMode}  toggleTheme={toggleTheme}  />
+      {matches && <CustomDrawer  open={open}  handleDrawerClose={handleDrawerClose} auth={auth}  user={user}  isDarkMode={isDarkMode}  toggleTheme={toggleTheme}  />}
       
-      <Box component="main" sx={{ flexGrow: 1 }} style={{ margin: '86px 0' }} >
+      <Box component="main" sx={{ flexGrow: 1 }} style={{ margin: '86px 0' }} > 
+      <Paper sx={{ width: '100%' }}> 
         <Outlet />
-      </Box>
+      </Paper>  
     </Box>
+    
+    </AppProvider> 
   );
 }
--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,21 +1,29 @@
 // WelcomePage.js
-import { Link } from 'react-router-dom';
 import FinancialReport from "../components/FinancialReport"; 
+
+import Button from '@mui/material/Button';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import Stack from '@mui/material/Stack';
+
 
 const WelcomePage = ({ auth, user, isDarkMode }) => {
   return (
     <div className="App"> 
       { auth ? <FinancialReport auth={auth} user={user} isDarkMode={isDarkMode} />
-      : (<div>
+      : (<>
         <h1>Добро пожаловать в приложение!</h1>
         <p>Здесь вы можете отслеживать свои доходы и управлять финансами.</p>
-        <Link to="/sign-in">
-          <button>Войти</button>
-        </Link>
-        <Link to="/sign-up">
-          <button>Зарегистрироваться</button>
-        </Link>
-        </div>)
+          
+        <Stack direction="row" spacing={2}>
+      <Button href="/sign-in" variant="contained" startIcon={<AccountCircleIcon color="secondary" fontSize="small" />}>
+        Личый кабинет
+      </Button>
+      <Button href="/sign-up" variant="outlined" startIcon={<TelegramIcon color="primary" fontSize="small" />}>
+        Получить доступ
+      </Button>
+    </Stack>
+        </>)
     } 
       </div>
   );
